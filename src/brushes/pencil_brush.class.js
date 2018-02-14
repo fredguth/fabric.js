1b1de0eade9d43c3c9977775ce667dcbc939a052
Fred Guth <fredguth@fredguth.com>
2018-02-14 18:35:57 -0200

    - prevents redrawing for each new point - ditches quadratic curves for cubic bezier curves

--- src/brushes/pencil_brush.class.js
+++ src/brushes/pencil_brush.class.js
@@ -15,6 +15,7 @@
     initialize: function(canvas) {
       this.canvas = canvas;
       this._points = [];
+      this._path = [];
     },
 
     /**
@@ -35,9 +36,6 @@
      */
     onMouseMove: function(pointer) {
       this._captureDrawingPath(pointer);
-      // redraw curve
-      // clear top canvas
-      this.canvas.clearContext(this.canvas.contextTop);
       this._render();
     },
 
@@ -79,6 +77,7 @@
      */
     _reset: function() {
       this._points.length = 0;
+      this._path.length = 0;
 
       this._setBrushStyles();
       this._setShadow();
@@ -94,44 +93,98 @@
     },
 
     /**
+     * One can think of a bezier curve in several ways. One way is to imagine
+     * it as an anchor point (also known as joint or knot) with 2 control points
+     * at each side, handle In and handle out. As we want a sequence of bezier curves
+     * that look "smooth", we need to construct handles with this property.
+     *
+     * This in known as the Continuity problem. Two curves in sequence
+     * B[t0-t1], p0p1, and B[t1-t2], p1p2, will look smooth if B'[t1] and B''[t1] at each
+     * curve is the same. In more technical terms, the function below find
+     * control points that satisfy G1 (the handles have the same slope, but not the same
+     * magnitude).
+     *
+     * A good explanation of this construction method can be found at:
+     * http://scaledinnovation.com/analytics/splines/aboutSplines.html
+     *
+     * @private
+     */
+    _getControlPoints: function(p0, p1, p2, t = 0.4) {
+      let h = p2.y - p0.y; // height of equilateral triangle with verticies p0, p2
+      let w = p2.x - p0.x; // width of same triangle
+
+      let d_01 = p1.distanceFrom(p0);
+      let d_12 = p2.distanceFrom(p1);
+
+      let fIn  = t * d_01 / (d_01 + d_12); // scaling factor for p1 Handle In
+      let fOut = t * d_12 / (d_01 + d_12); // same for Handle Out
+
+      let p1p2 = p2.subtract(p1);
+      let handleIn  = p1.subtract(p1p2.multiply(fIn));
+      let handleOut = p1.add(p1p2.multiply(fOut));
+      return { handleIn, handleOut };
+
+    },
+
+    /**
      * Draw a smooth path on the topCanvas using quadraticCurveTo
      * @private
      */
     _render: function() {
-      var ctx  = this.canvas.contextTop, i, len,
-          p1 = this._points[0],
-          p2 = this._points[1];
+      var ctx  = this.canvas.contextTop,
+          i,
+          len = this._points.length,
+          p0 = this._points[0],
+          p1 = this._points[1];
 
-      this._saveAndTransform(ctx);
+      if (!p0) {
+        return;
+      }
       ctx.beginPath();
+
+      ctx.moveTo(p0.x, p0.y);
+      this._path.push(`M ${p0.x} ${p0.y}`);
+
       //if we only have 2 points in the path and they are the same
       //it means that the user only clicked the canvas without moving the mouse
       //then we should be drawing a dot. A path isn't drawn between two identical dots
       //that's why we set them apart a bit
-      if (this._points.length === 2 && p1.x === p2.x && p1.y === p2.y) {
+      if (len === 2 && p0.x === p1.x && p0.y === p1.y) {
         var width = this.width / 1000;
-        p1 = new fabric.Point(p1.x, p1.y);
-        p2 = new fabric.Point(p2.x, p2.y);
-        p1.x -= width;
-        p2.x += width;
-      }
-      ctx.moveTo(p1.x, p1.y);
 
-      for (i = 1, len = this._points.length; i < len; i++) {
-        // we pick the point between pi + 1 & pi + 2 as the
-        // end point and p1 as our control point.
-        var midPoint = p1.midPointFrom(p2);
-        ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+        p0.x -= width;
+        p1.x += width;
+        ctx.lineTo(p1.x, p1.y);
+        this._path.push(`L ${p1.x} ${p1.y}`);
 
-        p1 = this._points[i];
-        p2 = this._points[i + 1];
+        // after using points, discard them.
+        this._points.shift();
       }
-      // Draw last line as a straight line while
-      // we wait for the next point to be able to calculate
-      // the bezier control point
-      ctx.lineTo(p1.x, p1.y);
+
+      if (len > 2) {
+        // need at least 4 points. If there is only 3, repeat first.
+        if (len == 3) {
+          this._points.unshift(this._points[0]);
+        }
+
+        var p2 = this._points[2],
+            p3 = this._points[3];
+
+        let p1_handles = this._getControlPoints(p0, p1, p2);
+        let p2_handles = this._getControlPoints(p1, p2, p3);
+
+        ctx.moveTo(p1.x, p1.y);
+        this._path.push(`M ${p1.x} ${p1.y}`);
+
+        ctx.bezierCurveTo(p1_handles.handleOut.x, p1_handles.handleOut.y, p2_handles.handleIn.x, p2_handles.handleIn.y, p2.x, p2.y);
+        this._path.push(`C ${p1_handles.handleOut.x} ${p1_handles.handleOut.y}, ${p2_handles.handleIn.x} ${p2_handles.handleIn.y},  ${p2.x} ${p2.y}`);
+
+        this._points.shift(); //keep only 4 points
+      }
+
+      ctx.closePath();
       ctx.stroke();
-      ctx.restore();
+
     },
 
     /**
@@ -140,35 +193,7 @@
      * @return {String} SVG path
      */
     convertPointsToSVGPath: function(points) {
-      var path = [], i, width = this.width / 1000,
-          p1 = new fabric.Point(points[0].x, points[0].y),
-          p2 = new fabric.Point(points[1].x, points[1].y),
-          len = points.length, multSignX = 1, multSignY = 1, manyPoints = len > 2;
-
-      if (manyPoints) {
-        multSignX = points[2].x < p2.x ? -1 : points[2].x === p2.x ? 0 : 1;
-        multSignY = points[2].y < p2.y ? -1 : points[2].y === p2.y ? 0 : 1;
-      }
-      path.push('M ', p1.x - multSignX * width, ' ', p1.y - multSignY * width, ' ');
-      for (i = 1; i < len; i++) {
-        if (!p1.eq(p2)) {
-          var midPoint = p1.midPointFrom(p2);
-          // p1 is our bezier control point
-          // midpoint is our endpoint
-          // start point is p(i-1) value.
-          path.push('Q ', p1.x, ' ', p1.y, ' ', midPoint.x, ' ', midPoint.y, ' ');
-        }
-        p1 = points[i];
-        if ((i + 1) < points.length) {
-          p2 = points[i + 1];
-        }
-      }
-      if (manyPoints) {
-        multSignX = p1.x > points[i - 2].x ? 1 : p1.x === points[i - 2].x ? 0 : -1;
-        multSignY = p1.y > points[i - 2].y ? 1 : p1.y === points[i - 2].y ? 0 : -1;
-      }
-      path.push('L ', p1.x + multSignX * width, ' ', p1.y + multSignY * width);
-      return path;
+      return this._path || [];
     },
 
     /**
